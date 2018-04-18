e13c0f5561cbc28ac9ddedd3497c06c359c97af4
kent0319 <kent0319.purple@gmail.com>
2018-04-18 21:49:05 +0900

    Display Microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,8 +8,11 @@ class UsersController < ApplicationController
 
 
 
+
+
   def show
-    @user = User.find(params[:id])
+      @user = User.find(params[:id])
+      @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def destroy
