c8b69267bcbf358865a72a802280c35780c88a28
kent0319 <kent0319.purple@gmail.com>
2018-04-20 21:44:52 +0900

    Add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
+  def feed
+     Micropost.where("user_id = ?", id)
+  end
 end
